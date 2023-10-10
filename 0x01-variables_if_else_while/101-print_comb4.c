513f3c27cee0daaca3882a22e9d25daeef95353e
Manleon47 <Mpholeon47@gmail.com>
2023-10-10 00:35:53 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+/**
+ * main - prints all possible different combinations of three digits.
+ * Numbers must be separated by ,, followed by a space.
+ * The three digits must be different.
+ * 012, 120, 102, 021, 201, 210 are considered the same combination.
+ * Print only the smallest combination of three digits.
+ * Numbers should be printed in ascending order, with three digits.
+ * Use the putchar function.
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+
+				if (i != 7 || j != 8 || k != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
